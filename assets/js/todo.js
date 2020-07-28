abb8c4a74c49fd5cf49cc87b10ab3799a406b7e7
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-07-28 19:18:26 +0200

    adding more important functions

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,24 +1,40 @@
-$('#item').keyup(function(event) {
+$('#typeItem').keyup(function(event) {
   if (event.which == '13') {
-    if ($('#item').val() == "" || $('#item').val().startsWith(" ") == true) {
+    if ($('#typeItem').val() == "" || $('#typeItem').val().startsWith(" ") == true) {
       validation();
     } else {
-      addingItem($('#item').val());
-      $('#item').val("");
+      var itemBody = $('#typeItem').val();
+      if($('#'+ itemBody).length >= 1) {
+        validation();
+      }
+      else {
+        addingItem($('#typeItem').val());
+        $('#typeItem').val("");
+      }
+
     }
   }
 });
 
 function addingItem(item) {
-
+  var element = $("<li></li>").text(item);
+  $(element).appendTo('#mainList');
+  $(element).attr('id', item);
+  $(element).addClass('oneItem');
+  $(element).attr('onclick', 'markDone(this)');
 }
 
 function validation() {
-  $('#item').val("");
-  $('#item').attr("placeholder", "Incorrectly entered value!");
-  $('#item').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  //tu będzie switch do warunków błędów 
+  $('#typeItem').val("");
+  $('#typeItem').attr("placeholder", "Incorrectly entered value!");
+  $('#typeItem').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   setTimeout(function() {
-    $('#item').attr("placeholder", "Tape your item here...");
+    $('#typeItem').attr("placeholder", "Tape your item here...");
   }, 600);
 
+}
+
+function markDone(item) {
+  $(item).toggleClass('itemDone');
 }
