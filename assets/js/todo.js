08dffe679ba860bcdf63c5dcc3b6ec34412c399f
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-08-25 20:32:40 +0200

    Next code normalization

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,24 +1,10 @@
 $('#typeItem').val('');
 const itemTable = [];
-$('#typeItem').keyup((event) => {
-  if (event.which == '13') {
-    if ($('#typeItem').val() == '') {
-      validation(0);
-    } else if ($('#typeItem').val().startsWith(' ') == true) {
-      validation(1);
-    } else if (itemTable.indexOf($('#typeItem').val()) >= 0) {
-      validation(2);
-    } else {
-      const itemBody = $('#typeItem').val();
-      addingItem($('#typeItem').val());
-      $('#typeItem').val('');
-    }
-  }
-});
 
-$(window).resize(() => {
-  mobileOnOff();
-});
+function checkMobile() {
+  if (window.innerWidth < window.innerHeight) return true;
+  return false;
+}
 
 function mobileOnOff() {
   if (checkMobile() == true) {
@@ -30,24 +16,13 @@ function mobileOnOff() {
   }
 }
 
-if (checkMobile() == true) {
-  const slipMainList = document.getElementById('mainList');
-  slipMainList.addEventListener('slip:swipe', (e) => {
-    if (isDone(e.target) == true) {
-      e.target.parentNode.removeChild(e.target);
-      deletingItem(e.target);
-    } else alert('This item is not ready!');
-  });
-  slipMainList.addEventListener('slip:reorder', (e) => {
-    e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
-  });
-
-  new Slip(slipMainList);
+function flashError() {
+  $('#typeItem').fadeOut(100).fadeIn(100).fadeOut(100)
+    .fadeIn(100);
+  setTimeout(() => {
+    $('#typeItem').attr('placeholder', 'Tape your item here...');
+  }, 800);
 }
-
-$('#cross').click(() => {
-  $('#typeItem').slideToggle('slow');
-});
 
 function validation(errCode) {
   $('#typeItem').val('');
@@ -64,15 +39,17 @@ function validation(errCode) {
       $('#typeItem').attr('placeholder', 'Value is already in the list!');
       flashError();
       break;
+    default:
+      $('#typeItem').attr('placeholder', 'Error 404!');
+      flashError();
   }
 }
 
-function flashError() {
-  $('#typeItem').fadeOut(100).fadeIn(100).fadeOut(100)
-    .fadeIn(100);
-  setTimeout(() => {
-    $('#typeItem').attr('placeholder', 'Tape your item here...');
-  }, 800);
+function addingDelButton(appToThis) {
+  const delButton = $('<span>✕</span>');
+  $(delButton).attr('id', 'delButton');
+  $(delButton).attr('onclick', 'deletingItem(this.parentElement)');
+  $(delButton).prependTo(appToThis);
 }
 
 function addingItem(item) {
@@ -94,12 +71,6 @@ function isDone(item) {
   if ($(item).hasClass('itemDone')) return true;
   return false;
 }
-function addingDelButton(appToThis) {
-  const delButton = $('<span>✕</span>');
-  $(delButton).attr('id', 'delButton');
-  $(delButton).attr('onclick', 'deletingItem(this.parentElement)');
-  $(delButton).prependTo(appToThis);
-}
 
 function deletingItem(itemToDelete) {
   if ($(itemToDelete).hasClass('itemDone')) {
@@ -112,7 +83,40 @@ function deletingItem(itemToDelete) {
   }
 }
 
-function checkMobile() {
-  if (window.innerWidth < window.innerHeight) return true;
-  return false;
+$(window).resize(() => {
+  mobileOnOff();
+});
+
+$('#cross').click(() => {
+  $('#typeItem').slideToggle('slow');
+});
+
+if (checkMobile() == true) {
+  const slipMainList = document.getElementById('mainList');
+  slipMainList.addEventListener('slip:swipe', (e) => {
+    if (isDone(e.target) == true) {
+      e.target.parentNode.removeChild(e.target);
+      deletingItem(e.target);
+    } else alert('This item is not ready!');
+  });
+  slipMainList.addEventListener('slip:reorder', (e) => {
+    e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
+  });
+
+  new Slip(slipMainList);
 }
+
+$('#typeItem').keyup((event) => {
+  if (event.which == '13') {
+    if ($('#typeItem').val() == '') {
+      validation(0);
+    } else if ($('#typeItem').val().startsWith(' ') == true) {
+      validation(1);
+    } else if (itemTable.indexOf($('#typeItem').val()) >= 0) {
+      validation(2);
+    } else {
+      addingItem($('#typeItem').val());
+      $('#typeItem').val('');
+    }
+  }
+});
