fd684dd11ee74bb1393832a2effd318697f3e440
Krystian Książek <47821226+krystianksiazek@users.noreply.github.com>
2020-08-25 20:13:26 +0200

    Code normalization

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,22 +1,22 @@
-$('#typeItem').val("");
-var itemTable = [];
-$('#typeItem').keyup(function(event) {
+$('#typeItem').val('');
+const itemTable = [];
+$('#typeItem').keyup((event) => {
   if (event.which == '13') {
-    if ($('#typeItem').val() == "") {
+    if ($('#typeItem').val() == '') {
       validation(0);
-    } else if ($('#typeItem').val().startsWith(" ") == true) {
+    } else if ($('#typeItem').val().startsWith(' ') == true) {
       validation(1);
     } else if (itemTable.indexOf($('#typeItem').val()) >= 0) {
       validation(2);
     } else {
-      var itemBody = $('#typeItem').val();
+      const itemBody = $('#typeItem').val();
       addingItem($('#typeItem').val());
-      $('#typeItem').val("");
+      $('#typeItem').val('');
     }
   }
 });
 
-$(window).resize(function() {
+$(window).resize(() => {
   mobileOnOff();
 });
 
@@ -31,52 +31,52 @@ function mobileOnOff() {
 }
 
 if (checkMobile() == true) {
-  var slipMainList = document.getElementById('mainList');
-  slipMainList.addEventListener('slip:swipe', function(e) {
-    if(isDone(e.target) == true) {
+  const slipMainList = document.getElementById('mainList');
+  slipMainList.addEventListener('slip:swipe', (e) => {
+    if (isDone(e.target) == true) {
       e.target.parentNode.removeChild(e.target);
       deletingItem(e.target);
-    }
-    else alert("This item is not ready!");
+    } else alert('This item is not ready!');
   });
-  slipMainList.addEventListener('slip:reorder', function(e) {
+  slipMainList.addEventListener('slip:reorder', (e) => {
     e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
   });
 
   new Slip(slipMainList);
 }
 
-$('#cross').click(function() {
-  $('#typeItem').slideToggle("slow");
+$('#cross').click(() => {
+  $('#typeItem').slideToggle('slow');
 });
 
 function validation(errCode) {
-  $('#typeItem').val("");
+  $('#typeItem').val('');
   switch (errCode) {
     case 0:
-      $('#typeItem').attr("placeholder", "Value is empty!");
+      $('#typeItem').attr('placeholder', 'Value is empty!');
       flashError();
       break;
     case 1:
-      $('#typeItem').attr("placeholder", "Value starts with space!");
+      $('#typeItem').attr('placeholder', 'Value starts with space!');
       flashError();
       break;
     case 2:
-      $('#typeItem').attr("placeholder", "Value is already in the list!");
+      $('#typeItem').attr('placeholder', 'Value is already in the list!');
       flashError();
       break;
   }
 }
 
 function flashError() {
-  $('#typeItem').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  setTimeout(function() {
-    $('#typeItem').attr("placeholder", "Tape your item here...");
+  $('#typeItem').fadeOut(100).fadeIn(100).fadeOut(100)
+    .fadeIn(100);
+  setTimeout(() => {
+    $('#typeItem').attr('placeholder', 'Tape your item here...');
   }, 800);
 }
 
 function addingItem(item) {
-  var element = $("<li></li>").text(item);
+  const element = $('<li></li>').text(item);
   $(element).appendTo('#mainList');
   $(element).attr('id', item);
   $(element).addClass('oneItem');
@@ -92,10 +92,10 @@ function markDone(item) {
 
 function isDone(item) {
   if ($(item).hasClass('itemDone')) return true;
-  else return false;
+  return false;
 }
 function addingDelButton(appToThis) {
-  var delButton = $("<span>✕</span>");
+  const delButton = $('<span>✕</span>');
   $(delButton).attr('id', 'delButton');
   $(delButton).attr('onclick', 'deletingItem(this.parentElement)');
   $(delButton).prependTo(appToThis);
@@ -103,16 +103,16 @@ function addingDelButton(appToThis) {
 
 function deletingItem(itemToDelete) {
   if ($(itemToDelete).hasClass('itemDone')) {
-    var deleter = itemToDelete.textContent;
+    let deleter = itemToDelete.textContent;
     deleter = deleter.substr(0, deleter.length);
     itemTable.splice(itemTable.indexOf(deleter), 1);
     $(itemToDelete).remove();
   } else {
-    alert("This item is not ready!");
+    alert('This item is not ready!');
   }
 }
 
 function checkMobile() {
   if (window.innerWidth < window.innerHeight) return true;
-  else return false;
+  return false;
 }
